e801baebc323e6804897055d059fc892f256ec96
jin <jchj108@naver.com>
2022-06-08 22:59:07 +0900

    템플릿, 컨트롤러 추가

--- src/main/java/com/jin/oembedtest/controller/OembedController.java
+++ src/main/java/com/jin/oembedtest/controller/OembedController.java
@@ -1,9 +1,37 @@
 package com.jin.oembedtest.controller;
 
+import com.jin.oembedtest.dto.Oembed;
+import com.jin.oembedtest.service.OembedService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpServletRequest;
 
 @Controller
 public class OembedController {
+
+//    private final OembedService oembedService;
+//
+//    public OembedController(OembedService oembedService) {
+//        this.oembedService = oembedService;
+//    }
+
+
+    @RequestMapping("/")
+    public String home() {
+        return "home";
+    }
+
+    @RequestMapping("/oembed/parsingOembed")
+    public String parsingOembed(HttpServletRequest request, @ModelAttribute Oembed oembed) {
+
+        System.out.println(oembed.getUrl());
+
+        return "index";
+    }
 
 
 
--- src/main/java/com/jin/oembedtest/service/OembedServiceImpl.java
+++ src/main/java/com/jin/oembedtest/service/OembedServiceImpl.java
@@ -1,5 +1,10 @@
 package com.jin.oembedtest.service;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class OembedServiceImpl implements OembedService{
+
+
 
 }
